8bdd31699ebbd1281c8f992f9a56543b12051f37
Liang YU <liang.yu@atos.net>
2018-06-22 11:32:33 +0200

    2 commit

--- src/main/java/KeyValueParser.java
+++ src/main/java/KeyValueParser.java
@@ -1,12 +1,13 @@
 package main.java;
 
+import org.apache.commons.lang.text.StrSubstitutor;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by a542901 on 22/06/2018.
@@ -14,31 +15,76 @@ import java.util.List;
 public class KeyValueParser {
 
     private static final String FILE_NAME = "config.properties";
+    private static final String PATH_PREFIX= "src/";
+    private static final String TEMPLATE_REGEX_PATTERN = "\\$\\{(.*?)}";
 
-    public static void main(String[] args) {
-        String path = "/configs/dev/east/node1";
-        List<String> pathList = new ArrayList(Arrays.asList(path.split("/")));
-        Iterator<String> it = pathList.iterator();
-        StringBuilder sb = new StringBuilder();
 
-        while (it.hasNext()){
-            String str = it.next();
-            if (str.equals("")){
-                it.remove();
+    public Map<String,String> getPathListForAllLevel(String inputStr) {
+
+        List<String> pathList = new ArrayList(Arrays.asList(inputStr.split("/")));
+        Iterator<String> iterator = pathList.iterator();
+        StringBuilder subPathSB = new StringBuilder().append(PATH_PREFIX);
+        //use treeMap to sort the properties in alphabetical order on key-name
+        Map<String,String> properties = new TreeMap<>();
+
+        while (iterator.hasNext()){
+            String str = iterator.next();
+            if ("".equals(str)){
+                iterator.remove();
             } else {
-                sb.append(str).append("/");
-                System.out.println(sb);
+                subPathSB.append(str).append("/");
+                if (Files.isReadable(Paths.get(subPathSB+FILE_NAME))) {
+                    try {
+                        List<String> strlist = Files.readAllLines(Paths.get(subPathSB+FILE_NAME));
+                        for (String string : strlist) {
+                            String[] keyValueArray = string.split("=");
+                            if (keyValueArray.length > 1) {
+                                //store the key as low case
+                                properties.put(keyValueArray[0].toLowerCase(), keyValueArray[1]);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
         }
 
+        System.out.println("properties before replacement of template:\n");
+        for (Map.Entry<String,String> entry: properties.entrySet()) {
+            System.out.println(entry.getKey() + "="+entry.getValue());
 
-
-        try {
-            List<String> strlist = Files.readAllLines(Paths.get("src/configs/config.properties"));
-            strlist.forEach(s -> System.out.println(s));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        Pattern pattern = Pattern.compile(TEMPLATE_REGEX_PATTERN);
+
+        //find and replace all the template ley & value
+        StrSubstitutor sub = new StrSubstitutor(properties);
+
+        for (Map.Entry<String,String> entry: properties.entrySet()) {
+            Matcher matcher =  pattern.matcher(entry.getValue());
+            while(matcher.find()) {
+                entry.setValue(sub.replace(entry.getValue()));
+            }
+
+        }
+
+        System.out.println("===========================================");
+        System.out.println("properties after replacement of template:\n");
+        for (Map.Entry<String,String> entry : properties.entrySet()) {
+            System.out.println(entry.getKey() + "="+entry.getValue());
+        }
+
+        return properties;
+    }
+
+
+    public static void main(String[] args) {
+        //INPUT
+        String inputStr = "/configs/dev/east/node1";
+        KeyValueParser keyValueParser = new KeyValueParser();
+        Map<String, String> properties = keyValueParser.getPathListForAllLevel(inputStr);
 
     }
 }
---
+++ src/test/java/KeyValueParserTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by a542901 on 22/06/2018.
+ */
+public class KeyValueParserTest extends TestCase {
+
+    public void testGetPathListForAllLevel() throws Exception {
+
+    }
+
+    public void testMain() throws Exception {
+
+    }
+}
